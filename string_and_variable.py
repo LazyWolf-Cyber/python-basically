6709a7b77b58f4c01b412ae7195c6c5723a27f39
Gh-ost <70215879+LazyWolf-Cyber@users.noreply.github.com>
2021-01-01 06:18:16 +0700

    Added comments for learning easier.

--- string_and_variable.py
+++ string_and_variable.py
@@ -1,4 +1,9 @@
+#The words in the brackets is the string,and we use print words for show the string to the terminal.
 print(" me:hello you")
+
+#The code below me is the example of the strings
+#'myFood' is the name of the variable,but words like str,print,and etc can't be variable
+#Inside variable,you can store strings,and another variable.Its cool!.
 myFood='indomie'
 aRobbery=" rampok:aku telah mencuri " + myFood + (" mu")
 print(aRobbery)
